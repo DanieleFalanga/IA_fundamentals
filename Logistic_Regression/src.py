dbaea64a107822a590972f853e284b30676035d5
DanieleFalanga <spadaccino2000@gmail.com>
2022-05-05 18:00:28 +0200

    Logistica di Prova per Flavio e Francesco

--- Logistic_Regression/src.py
+++ Logistic_Regression/src.py
@@ -1,46 +1,40 @@
 from sklearn.datasets import load_iris
 import numpy as np
 
-ALPHA = 0.0001 
+ALPHA = 0.001 
 
-def total_value_binary_cross_entropy(y,h):
-    loss_or_cost = 0
+def binary_cross_entropy(y,theta):
+    cost = 0
     for i in range(0,len(y)):
-        loss_or_cost += (-(y[i]*np.log(h[i]) + (1-y[i])*np.log(1-h[i])))
-    return loss_or_cost
-
-def single_value_binary_cross_entropy(y_i, h_i):
-    loss_or_cost = -(y_i*np.log(h_i) + (1-y_i)*np.log(1-h_i))
-    return loss_or_cost
+        cost += (-(y[i]*np.log(theta[i]) + (1-y[i])*np.log(1-theta[i])))
+    return cost
 
 def Logistic_Regression(w,y,x):
     #scansiono colonne di x
     previus_cost = None
     while(True):
         exp = exponent(x,w)
-        hyp = sigmoid(exp)
-        current_cost = total_value_binary_cross_entropy(y,hyp)
+        hyp = theta(exp)
+        current_cost = binary_cross_entropy(y,hyp)
         print(current_cost)
         if previus_cost and current_cost > previus_cost:
             print("Loss Totale: ", previus_cost)
             break
         previus_cost = current_cost
-        print(previus_cost)
         for j in range(0,len(x[0])):
             # scansiono le righe di x
             for i in range(0,len(x)):
-                w[j] += ALPHA*(y[i]-hyp[i])*hyp[i]*(1-hyp[i])*x[i]
+                w[j] += ALPHA*(y[i]-hyp[i])*x[i][j]
         
     return 
 
 
 def exponent(x,w): 
-    return np.dot(x,w)
+    return np.dot(x,np.transpose(w))
 
-def sigmoid(z):
+def theta(z):
     res = 1/(1+np.exp(-z))
     return res
-
 
 def main():
     data = load_iris()
@@ -77,7 +71,7 @@ def main():
     w = [1,1,1,1,1]
     Logistic_Regression(w,y_trainingset, training_set)
     
-    #print(w)
+    print(w)
     
 
 
